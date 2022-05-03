7eb144a665a13fc986d3c5cec046320cc1eb0a36
fomenkosergey1987 <fomenkosergey1987@gmail.com>
2022-05-03 09:49:40 +0300

    add src

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+import '../scss/main.scss';
+// import {$, jQuery} from 'jquery';
+// // export for others scripts to use
+// window.$ = $;
+// window.jQuery = jQuery;
+
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+
+/*TODO: need import bootstar and jqery*/
+jQuery(document).ready(function($){
+    console.log('JQuery is ready!!!!!');
+})
+/* Your JS Code goes here */
+// $('#register-now-window').on('shown.bs.modal', function () {
+//     $('#register-now-button').trigger('focus')
+// })
+/* Demo JS */
+//import './demo.js';
